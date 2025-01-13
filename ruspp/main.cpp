dc5e7600229f4e65ef1d55b3f5c6eb7bd0f578d8
d_cuzya <d.cuzya07@gmail.com>
2025-01-13 09:25:47 +0300

    first update

--- ruspp/main.cpp
+++ ruspp/main.cpp
@@ -5,22 +5,21 @@
 int main(void)
 {
     GLFWwindow* window;
-    // Initialize GLFW
     if (!glfwInit()) {
         return 1;
     }
-    // Create window
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return 1;
     }
-    glfwMakeContextCurrent(window); // Create Context
-    while (!glfwWindowShouldClose(window)) // Main loop
+    // Set scale here
+    float scale = 0.2f;
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
     {
-        float scale = 0.2f;
-        glClear(GL_COLOR_BUFFER_BIT); // Clear buffer (GL_COLOR_BUFFER_BIT - clear color)
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_LINES);
         glColor3f(0.0f, 1.0f, 0.0f);
@@ -34,6 +33,7 @@ int main(void)
         glColor3f(1.0f, 1.0f, 1.0f);
         for (float x = -5; x <= 5; x+=0.001f)
         {
+            // Set function here
             float y = 0.2f/x;
             glVertex2f(x* scale, y* scale);
         }
@@ -47,8 +47,8 @@ int main(void)
         glVertex2f(-1* scale, 0* scale);
         glEnd();
 
-        glfwSwapBuffers(window); // Swap previous and buffers
-        glfwPollEvents(); // Processing system and other events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     glfwTerminate();
     return 0;
